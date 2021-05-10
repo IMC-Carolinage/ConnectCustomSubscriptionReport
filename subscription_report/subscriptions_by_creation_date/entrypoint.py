045c50846c7f90232d02d2ffc0951c0fcce09f4d
IMC-Carolinage <Carolina.GimenezEscalante@ingrammicro.com>
2021-05-10 15:55:15 +0200

    First version adding the reconciliation parameter column.

--- subscription_report/subscriptions_by_creation_date/entrypoint.py
+++ subscription_report/subscriptions_by_creation_date/entrypoint.py
@@ -3,6 +3,11 @@
 # Copyright (c) 2021, Carolina Giménez Escalante
 # All rights reserved.
 #
+
+from cnct import R
+from .utils import convert_to_datetime, get_basic_value, get_value, today_str
+
+
 def generate(client, parameters, progress_callback):
     """
     Extracts data from Connect using the ConnectClient instance
@@ -11,10 +16,9 @@ def generate(client, parameters, progress_callback):
     that will be used to fill the Excel file.
     Each element returned by the iterator must be an iterator over
     the columns value.
-
     :param client: An instance of the CloudBlue Connect
                     client.
-    :type client: cnct.ConnectClient
+    :type client: connect.client.ConnectClient
     :param parameters: Input parameters used to calculate the
                         resulting dataset.
     :type parameters: dict
@@ -24,4 +28,80 @@ def generate(client, parameters, progress_callback):
                                 of the report generation.
     :type progress_callback: func
     """
-    pass
+    requests = _get_requests(client, parameters)
+
+    progress = 0
+    total = requests.count()
+
+    for request in requests:
+        connection = request['asset']['connection']
+        reconciliation_param_value = ''
+
+        for param in request['asset']['params']:
+            if param['reconciliation']:
+                reconciliation_param_value = get_basic_value(param, 'value')
+
+        for item in request['asset']['items']:
+            yield (
+                get_basic_value(request, 'id'),
+                get_basic_value(request, 'type'),
+                convert_to_datetime(
+                    get_basic_value(request, 'created'),
+                ),
+                convert_to_datetime(
+                    get_basic_value(request, 'updated'),
+                ),
+                today_str(),
+                get_basic_value(item, 'global_id'),
+                get_basic_value(item, 'display_name'),
+                get_basic_value(item, 'item_type'),
+                get_basic_value(item, 'type'),
+                get_basic_value(item, 'mpn'),
+                get_basic_value(item, 'period'),
+                get_basic_value(item, 'quantity'),
+                get_value(request['asset']['tiers'], 'customer', 'id'),
+                get_value(request['asset']['tiers'], 'customer', 'name'),
+                get_value(request['asset']['tiers'], 'customer', 'external_id'),
+                get_value(request['asset']['tiers'], 'tier1', 'id'),
+                get_value(request['asset']['tiers'], 'tier1', 'name'),
+                get_value(request['asset']['tiers'], 'tier1', 'external_id'),
+                get_value(request['asset']['tiers'], 'tier2', 'id'),
+                get_value(request['asset']['tiers'], 'tier2', 'name'),
+                get_value(request['asset']['tiers'], 'tier2', 'external_id'),
+                get_value(request['asset']['connection'], 'provider', 'id'),
+                get_value(request['asset']['connection'], 'provider', 'name'),
+                get_value(request['asset']['connection'], 'vendor', 'id'),
+                get_value(request['asset']['connection'], 'vendor', 'name'),
+                get_value(request['asset'], 'product', 'id'),
+                get_value(request['asset'], 'product', 'name'),
+                get_value(request, 'asset', 'id'),
+                get_value(request, 'asset', 'external_id'),
+                reconciliation_param_value,
+                get_value(request['asset'], 'connection', 'type'),
+                get_value(connection, 'hub', 'id') if 'hub' in connection else '',
+                get_value(connection, 'hub', 'name') if 'hub' in connection else '',
+                get_value(request, 'asset', 'status'),
+            )
+        progress += 1
+        progress_callback(progress, total)
+
+
+def _get_requests(client, parameters):
+    all_types = ['tiers_setup', 'inquiring', 'pending', 'approved', 'failed', 'draft']
+
+    query = R()
+    query &= R().created.ge(parameters['date']['after'])
+    query &= R().created.le(parameters['date']['before'])
+
+    if parameters.get('product') and parameters['product']['all'] is False:
+        query &= R().asset.product.id.oneof(parameters['product']['choices'])
+    if parameters.get('rr_type') and parameters['rr_type']['all'] is False:
+        query &= R().type.oneof(parameters['rr_type']['choices'])
+    if parameters.get('rr_status') and parameters['rr_status']['all'] is False:
+        query &= R().status.oneof(parameters['rr_status']['choices'])
+    else:
+        query &= R().status.oneof(all_types)
+    if parameters.get('mkp') and parameters['mkp']['all'] is False:
+        query &= R().asset.marketplace.id.oneof(parameters['mkp']['choices'])
+
+    return client.requests.filter(query)
