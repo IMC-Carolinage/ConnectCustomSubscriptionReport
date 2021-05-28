20fd13c23fff6f416034ec92fbd1dbc9dd7db119
IMC-Carolinage <80520537+IMC-Carolinage@users.noreply.github.com>
2021-05-28 13:55:04 +0200

    Update test_subscriptionsreportbycreationdate.py

--- tests/test_subscriptionsreportbycreationdate.py
+++ tests/test_subscriptionsreportbycreationdate.py
@@ -12,8 +12,8 @@ def test_subscriptions_by_creation_date(progress, client_factory, response_facto
 
     parameters = {
         "date": {
-            "after": "2020-12-01T00:00:00",
-            "before": "2021-01-01T00:00:00",
+            "after": "2021-01-01T00:00:00",
+            "before": "2021-12-01T00:00:00",
         },
         "product": {
             "all": True,
@@ -23,18 +23,10 @@ def test_subscriptions_by_creation_date(progress, client_factory, response_facto
             "all": True,
             "choices": [],
         },
-        "rr_type": {
-            "all": True,
-            "choices": [],
-        },
         "mkp": {
             "all": True,
             "choices": [],
-        },
-        "hub": {
-            "all": True,
-            "choices": [],
-        },
+        }
     }
     responses.append(
         response_factory(
@@ -44,7 +36,7 @@ def test_subscriptions_by_creation_date(progress, client_factory, response_facto
 
     responses.append(
         response_factory(
-            query='and(ge(created,2020-12-01T00:00:00),le(created,2021-01-01T00:00:00),in(status,'
+            query='and(ge(created,2021-01-01T00:00:00),le(created,2021-12-01T00:00:00),in(status,'
                   '(tiers_setup,inquiring,pending,approved,failed,draft)))',
         ),
     )
@@ -61,8 +53,8 @@ def test_generate_additional(progress, client_factory, response_factory, ff_requ
 
     parameters = {
         "date": {
-            "after": "2020-12-01T00:00:00",
-            "before": "2021-01-01T00:00:00",
+            "after": "2021-01-01T00:00:00",
+            "before": "2021-12-01T00:00:00"
         },
         "product": {
             "all": False,
@@ -74,18 +66,10 @@ def test_generate_additional(progress, client_factory, response_factory, ff_requ
             "all": False,
             "choices": ['approved'],
         },
-        "rr_type": {
-            "all": False,
-            "choices": ['purchase'],
-        },
         "mkp": {
             "all": False,
             "choices": ['MP-123'],
-        },
-        "hub": {
-            "all": False,
-            "choices": ['HB-123'],
-        },
+        }
     }
     responses.append(
         response_factory(
@@ -95,10 +79,9 @@ def test_generate_additional(progress, client_factory, response_factory, ff_requ
 
     responses.append(
         response_factory(
-            query='and(ge(created,2020-12-01T00:00:00),le(created,2021-01-01T00:00:00),'
-                  'in(asset.product.id,(PRD-276-377-545)),in(type,(purchase)),in(status,'
-                  '(approved)),in(asset.marketplace.id,(MP-123)),in(asset.connection.hub.id,'
-                  '(HB-123)))',
+            query='and(ge(created,2021-01-01T00:00:00),le(created,2021-01-31T00:00:00),'
+                  'in(asset.product.id,(PRD-276-377-545)),in(status,'
+                  '(approved)),in(asset.marketplace.id,(MP-123))))',
             value=[ff_request],
         ),
     )
